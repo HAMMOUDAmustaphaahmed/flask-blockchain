2b774f7a3e3adc1d68f18682794127029d46537d
HAMMOUDAmustaphaahmed <hammouda.ahmedmustapha@gmail.com>
2024-05-20 17:12:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+Blockchain simulation with a web interface on Flask.
+
+**Blockchain** –  is a continuously growing list of records, called blocks, which are linked and secured using cryptography hash. Each block typically contains a hash pointer as a link to a previous block and a timestamp. By design, blockchains are inherently resistant to modification of the data. For getting hash used _SHA-256_
+
+### What is POW
+
+**POW** _(proof of work)_ is a piece of data which is difficult (costly, time-consuming) to produce but easy for others to verify and which satisfies certain requirements. Producing a proof of work can be a random process with low probability so that a lot of trial and error is required on average before a valid proof of work is generated. Bitcoin uses the Hashcash proof of work system.
+
+## How to run
+
+```
+pip install -r requirements.txt
+
+python server.py
+```
+
+Now head over to http://127.0.0.1:5000/, and you should see main page of blockchain, where you can add new block, check blocks integrity and mined blocks using POW algorithm.
+
+
---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import render_template, redirect, url_for
+from flask import request
+import blockchain
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        text = request.form["text"]
+        if len(text) < 1:
+            return redirect(url_for("index"))
+
+        make_proof = request.form.get("make_proof", False)
+        blockchain.write_block(text, make_proof)
+        return redirect(url_for("index"))
+    return render_template("index.html")
+
+
+@app.route("/check", methods=["POST"])
+def integrity():
+    results = blockchain.check_blocks_integrity()
+    if request.method == "POST":
+        return render_template("index.html", results=results)
+    return render_template("index.html")
+
+
+@app.route("/mining", methods=["POST"])
+def mining():
+    if request.method == "POST":
+        max_index = int(blockchain.get_next_block())
+
+        for i in range(2, max_index):
+            blockchain.get_POW(i)
+        return render_template("index.html", querry=max_index)
+    return render_template("index.html")
+
+@app.route("/blocks", methods=["GET"])
+def list_blocks():
+    blocks = blockchain.get_all_blocks()
+    return render_template("blocks.html", blocks=blocks)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
